c726e065f857d2e58dfed42d4d1df1ce3d916fea
JueZ <mkoschi@me.com>
2014-01-22 00:20:13 +0100

    new files

---
+++ PDS/SKS_PDS_Tests/PackageServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SKS_PDS.DAL;
+using SKS_PDS.Entities;
+using SKS_PDS.Services;
+
+using SKS_PDS_Tests.ShippingService;
+
+namespace SKS_PDS_Tests
+{
+    [TestClass]
+    public class PackageServiceTest
+    {
+        [TestMethod]
+        public void AddPackage_importdata_PackageAdded()
+        {
+            //arrange
+            ShippingService.ShippingServiceClient shippingserviceclient = new ShippingServiceClient();
+            //act
+            ShippingService.Package package = new ShippingService.Package();
+
+            package.Address = new ShippingService.Address();
+
+            package.Address.City = "Vienna";
+            package.Address.Country = "Austria";
+            package.Address.PostalCode = "1010";
+            package.Address.Street = "Kärntnerstraße";
+            package.Address.Id = 1;
+            package.Id = 1;
+            
+            shippingserviceclient.AddPackage(package);
+
+            //assert
+            Assert.IsFalse(false);
+        }
+    }
+}
---
+++ PDS/TestXMLImport/Program.cs
@@ -0,0 +1,25 @@
+﻿using SKS_PDS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestXMLImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrange
+            ServiceController servicecontroller = new ServiceController();
+            servicecontroller.StartService();
+
+            Console.ReadKey();
+            //act
+
+            //assert
+
+        }
+    }
+}
